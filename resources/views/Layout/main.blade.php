32adefd537ecbf2f4b035e877a8ea8d3d7db010b
= <xoliqulovmuhammadnabi842@gmail.com>
2024-11-15 12:08:40 +0500

    Updated

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -128,15 +128,6 @@
                         </li>
                         
                         <li class="nav-item">
-                            <a href="/users" class="nav-link">
-                                <i class="nav-icon fas fa-bars"></i>
-                                <p>
-                                    Users
-                                </p>
-                            </a>
-                        </li>
-                       
-                        <li class="nav-item">
                             <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 <i class="nav-icon fas fa-bars"></i>
                                 <p>Logout</p>
